4a6fc4d4da5ad6b65a4704e0f010b4093c944384
Roger Ng <roger2hk@gmail.com>
2017-03-05 22:45:36 +0800

    fix(pager): prevent page reload in IE when anchor tag is disabled fix(pagination): prevent page reload in IE when anchor tag is disabled

--- src/tabindex/tabindex.js
+++ src/tabindex/tabindex.js
@@ -3,9 +3,16 @@ angular.module('ui.bootstrap.tabindex', [])
 .directive('uibTabindexToggle', function() {
   return {
     restrict: 'A',
+    scope: {
+      tabindexDisabled: '=?'
+    },
     link: function(scope, elem, attrs) {
       attrs.$observe('disabled', function(disabled) {
         attrs.$set('tabindex', disabled ? -1 : null);
+      });
+
+      scope.$watch('tabindexDisabled', function(value) {
+        attrs.$set('tabindex', value ? -1 : null);
       });
     }
   };
